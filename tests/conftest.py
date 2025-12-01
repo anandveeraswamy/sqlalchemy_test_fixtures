64a5e21e111ece80c08cdd1ad19546daab1423a7
Anand <anandveeraswamy@gmail.com>
2025-12-01 21:17:16 +0000

    added __init__.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,6 @@
 import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
-import sys
-from pathlib import Path
-ROOT_DIR = Path(__file__).resolve().parents[1]
-sys.path.insert(0, str(ROOT_DIR))
-
 from models import Base
 
 
